e94690c9be877d31a59956c422b2fe619869d879
Nina Zakharenko <nzakharenko@gmail.com>
2020-10-30 00:47:36 -0700

    Fix view name

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from blog import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.index, name="posts"),
     path("about/", views.about, name="about"),
     path("<slug:slug>/", views.post, name="post"),
 ]
